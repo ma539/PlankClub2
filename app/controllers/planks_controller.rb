d85420c62c37450bcf45d16ac1fb61f9ce2c8da7
ma539 <ma539@cornell.edu>
2018-08-22 11:21:20 -0400

    Plank methods complete

--- app/controllers/planks_controller.rb
+++ app/controllers/planks_controller.rb
@@ -8,28 +8,39 @@ class PlanksController < ApplicationController
   end
 
   def new
-    
+    @plank = Plank.new
   end
 
   def create
-
+    @plank = Plank.new(plank_params)
+    if @plank.save
+      redirect_to plank_path(@plank)
+    else
+      render :new
+    end
   end
 
   def edit
-
+    @plank = Plank.find(params[:id])
   end
 
   def update
-
+    @plank = Plank.find(params[:id])
+    if @plank.update(plank_params)
+      redirect_to plank_path(@plank)
+    else
+      render :edit
+    end
   end
 
-  def delete
-
+  def destroy
+    @plank = Plank.find(params[:id])
+    @plank.destroy
+    redirect_to planks_path
   end
 
   private
-
   def plank_params
-    params.require(:plank).permit(:type, :duration)
+    params.require(:plank).permit(:planktype)
   end
 end
